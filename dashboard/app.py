96da98bbf02109aa022f3738f6c1e5af07c06a37
drodmay1 <drodmay@gmail.com>
2024-04-02 17:04:46 -0400

    app updated with total value

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,11 +8,10 @@ from shiny.express import input, render, ui
 from shiny.ui import output_ui
 from shinywidgets import render_plotly
 from stocks import stocks
-import plotly.express as px
 
-# Default to the last 2 months
+# Default to the last month
 end = pd.Timestamp.now()
-start = end - pd.Timedelta(weeks=8)
+start = end - pd.Timedelta(weeks=4)
 
 ui.page_opts(title="Apple AAPL Stock Explorer", fillable=True)
 selected_stock = "AAPL" #default selected stock
@@ -22,7 +21,7 @@ with ui.sidebar():
     ui.input_text("search", "Search Stock e.g. AAPL", placeholder="Enter stock symbol")
     ui.input_selectize("ticker", "Select Stocks", choices=stocks, selected="AAPL")
     ui.input_date_range("dates", "Select dates", start=start, end=end)
-    ui.input_numeric("quantity", "Quantity - Total Value", min=1, max=100, step=1, value=1)
+    ui.input_numeric("quantity", "Quantity", min=1, max=100, step=1, value=1)
 
 
 with ui.layout_column_wrap(fill=False):
@@ -47,54 +46,45 @@ with ui.layout_column_wrap(fill=False):
         @render.ui
         def change_percent():
             return f"{get_change_percent():.2f}%"
-    
+        
+    with ui.value_box(showcase=icon_svg("coins")):
+        "Total Value"
 
-with ui.layout_columns(col_widths=[6, 6]):
-    with ui.card():
+        @render.ui
+        def total_value():
+            data = get_data()
+            quantity = input.quantity()
+            if data.empty or quantity is None:
+                return "N/A"
+            else:
+                close_price = data["Close"].iloc[-1]
+                return f"${close_price * quantity:.2f}"
+            
+        ui.include_css(Path(__file__).parent / "styles.css")
+
+
+with ui.layout_columns(col_widths=[9, 3]):
+    with ui.card(full_screen=True):
         ui.card_header("Price history")
 
         @render_plotly
         def price_history():
             qf = cf.QuantFig(
                 get_data(),
-                name =input.ticker(),
-                up_color="#44bb70",
-                down_color="#040548",
-                legend="top",
+                name=input.ticker(),
+                up_color="#00C853",
+                down_color="#FF4747",
+                legend="Outside",
             )
-            qf.add_sma()
-            qf.add_volume(up_color="#44bb70", down_color="#040548")
+            qf.add_sma()  
+            qf.add_volume(up_color="#00C853", down_color="#FF4747")
             fig = qf.figure()
             fig.update_layout(
                 hovermode="x unified",
-                paper_bgcolor="rgba(0,0,0,0)",
-                plot_bgcolor="rgba(0,0,0,0)",
-                width=800,  # Adjust width as needed
-                height=400
+                paper_bgcolor="rgba(255,255,255,0.8)",  # Light background
+                plot_bgcolor="rgba(255,255,255,0.8)",
             )
             return fig
-        
-    # Second chart (for volume)
-    with ui.layout_columns(col_widths=[12]):
-        with ui.card():
-            ui.card_header("Volume")  # Card header for the second chart
-
-            @render_plotly
-            def volume_chart():
-                volume_data = get_data()["Volume"]
-                fig = volume_data.iplot(asFigure=True, kind="bar")
-                fig.update_layout(
-                    title="Volume",
-                    xaxis_title="Date",
-                    yaxis_title="Volume",
-                    hovermode="x unified",
-                    paper_bgcolor="rgba(0,0,0,0)",
-                    plot_bgcolor="rgba(0,0,0,0)",
-                    width=800,  # Adjust width as needed
-                    height=400  # Adjust height as needed
-                )
-                return fig
-        
 
     with ui.card():
         ui.card_header("Latest data")
@@ -106,22 +96,6 @@ with ui.layout_columns(col_widths=[6, 6]):
             x["Value"] = x["Value"].apply(lambda v: f"{v:.1f}")
             return x
         
-    with ui.card():
-        ui.card_header(icon_svg("chart-line"), "Total Value")
-
-        @render.ui
-        def total_value():
-            data = get_data()
-            quantity = input.quantity()
-            if data.empty or quantity is None:
-                return "N/A"
-            else:
-                close_price = data["Close"].iloc[-1]
-                return f"${close_price * quantity:.2f}"
-
-
-ui.include_css(Path(__file__).parent / "styles.css")
-
 # Reactive calculations
 
 @reactive.calc
@@ -138,7 +112,6 @@ def get_data():
 def get_change():
     close = get_data()["Close"]
     return close.iloc[-1] - close.iloc[-2]
-
 
 @reactive.calc
 def get_change_percent():
